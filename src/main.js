514da97057272634e642963f3f059ec730f16bf5
neel <neel@rocketbots.io>
2021-01-14 12:28:54 +0800

    added products

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
+import _ from 'lodash';
+import VueTruncate from 'vue-truncate-filter';
+
+Vue.use(VueTruncate)
+Vue.prototype._ = _;
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,10 @@
                         justify="center"
                     >
                         <v-col>
-                            <div class="display-2 white--text px-12 hidden-sm-only text-shadow"><strong>{{ banner.title }}</strong></div><br/>
+                            <div class="display-2 white--text px-12 hidden-sm-only text-shadow"><strong>{{
+                                    banner.title
+                                }}</strong></div>
+                            <br/>
                         </v-col>
                     </v-row>
                 </v-carousel-item>
@@ -53,21 +56,212 @@
                 </v-row>
             </v-col>
         </v-row>
+        <v-row no-gutters align="center" justify="center" class="pt-2 px-2 pb-0">
+            <h1 class="font-weight-light text-center">Featured Items</h1>
+        </v-row>
+        <v-row no-gutters justify="space-around" class="pa-2 pt-4">
+            <v-col cols="6" md="3" class="pa-2" v-for="item in featured" :key="`featured-${item.id}`">
+                <v-card
+                    class="pa-1"
+                >
+                    <v-img
+                        height="150"
+                        contain
+                        :src="item.image"
+                    />
+
+                    <v-card-title style="min-height: 140px">{{ item.title | truncate(50) }}</v-card-title>
+
+                    <v-card-text>
+                        <v-row
+                            align="center"
+                            class="mx-0"
+                        >
+                            <v-rating
+                                :value="4.5"
+                                color="amber"
+                                dense
+                                half-increments
+                                readonly
+                                size="14"
+                            ></v-rating>
+
+                            <div class="grey--text ml-4">
+                                4.5 (413)
+                            </div>
+                        </v-row>
+
+                        <div class="my-4 subtitle-1 primary--text">
+                            From: ${{ item.price }}
+                        </div>
+
+                        <div style="min-height: 90px">{{ item.description | truncate(100) }}</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn
+                            color="primary lighten-2"
+                            text
+                            @click="book"
+                        >
+                            Book Now
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row no-gutters align="center" justify="center" class="py-5">
+            <v-btn color="primary">Show More</v-btn>
+        </v-row>
+        <v-divider/>
+
+        <v-row no-gutters align="center" justify="center" class="pt-2 px-2 pb-0">
+            <h1 class="font-weight-light text-center">Top Items of the week</h1>
+        </v-row>
+        <v-row no-gutters justify="space-around" class="pa-2 pt-4">
+            <v-col cols="6" md="3" class="pa-2" v-for="item in top_items" :key="`top_items-${item.id}`">
+                <v-card
+                    min-height="150"
+                    class="pa-1"
+                >
+                    <v-img
+                        height="150"
+                        contain
+                        :src="item.image"
+                    />
+
+                    <v-card-title style="min-height: 140px">{{ item.title | truncate(50) }}</v-card-title>
+
+                    <v-card-text>
+                        <v-row
+                            align="center"
+                            class="mx-0"
+                        >
+                            <v-rating
+                                :value="4.5"
+                                color="amber"
+                                dense
+                                half-increments
+                                readonly
+                                size="14"
+                            ></v-rating>
+
+                            <div class="grey--text ml-4">
+                                4.5 (413)
+                            </div>
+                        </v-row>
+
+                        <div class="my-4 subtitle-1 primary--text">
+                            From: ${{ item.price }}
+                        </div>
+
+                        <div style="min-height: 90px">{{ item.description | truncate(100) }}</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn
+                            color="primary lighten-2"
+                            text
+                            @click="book"
+                        >
+                            Book Now
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row no-gutters align="center" justify="center" class="py-5">
+            <v-btn color="primary">Show More</v-btn>
+        </v-row>
+        <v-divider/>
+
+        <v-row no-gutters align="center" justify="center" class="pt-2 px-2 pb-0">
+            <h1 class="font-weight-light text-center">New Arrival</h1>
+        </v-row>
+        <v-row no-gutters justify="space-around" class="pa-2 pt-4">
+            <v-col cols="6" md="3" class="pa-2" v-for="item in new_releases" :key="`new_releases-${item.id}`">
+                <v-card
+                    min-height="150"
+                    class="pa-1"
+                >
+                    <v-img
+                        height="150"
+                        contain
+                        :src="item.image"
+                    />
+
+                    <v-card-title style="min-height: 140px">{{ item.title | truncate(50) }}</v-card-title>
+
+                    <v-card-text>
+                        <v-row
+                            align="center"
+                            class="mx-0"
+                        >
+                            <v-rating
+                                :value="4.5"
+                                color="amber"
+                                dense
+                                half-increments
+                                readonly
+                                size="14"
+                            ></v-rating>
+
+                            <div class="grey--text ml-4">
+                                4.5 (413)
+                            </div>
+                        </v-row>
+
+                        <div class="my-4 subtitle-1 primary--text">
+                            From: ${{ item.price }}
+                        </div>
+
+                        <div style="min-height: 90px">{{ item.description | truncate(100) }}</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn
+                            color="primary lighten-2"
+                            text
+                            @click="book"
+                        >
+                            Book Now
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row no-gutters align="center" justify="center" class="py-5">
+            <v-btn color="primary">Show More</v-btn>
+        </v-row>
+        <v-divider/>
+
         <v-row color="grey" no-gutters class="grey--text">
             <v-col cols="12" md="4" class="pa-8 text-justify">
-                RentSmart Asia “(RentSmart Asia Sdn Bhd (1267397-U)” is changing the way rentals are done. We offer an e-commerce online rental platform for anyone across Malaysia that is fast, secure and convenient. Our online stores offer a broad product offering in categories ranging from cameras, laptops, computers, mobile phones, tablets, to household goods, toys, baby equipment to gowns, wedding dresses, costumes, and sports equipment.
+                RentSmart Asia “(RentSmart Asia Sdn Bhd (1267397-U)” is changing the way rentals are done. We offer an
+                e-commerce online rental platform for anyone across Malaysia that is fast, secure and convenient. Our
+                online stores offer a broad product offering in categories ranging from cameras, laptops, computers,
+                mobile phones, tablets, to household goods, toys, baby equipment to gowns, wedding dresses, costumes,
+                and sports equipment.
             </v-col>
             <v-col cols="12" md="4" class="pa-8 text-justify">
-                RentSmart is always striving to offer our customers the best possible offering – including multiple payment options, multiple delivery options, extensive customer service and insurance against accidental damage and theft. We are constantly expanding our product range as well like recently the Fashion category. We offer the vast selection of products that are vetted for the best quality that you’ll find anywhere all at your fingertips.
+                RentSmart is always striving to offer our customers the best possible offering – including multiple
+                payment options, multiple delivery options, extensive customer service and insurance against accidental
+                damage and theft. We are constantly expanding our product range as well like recently the Fashion
+                category. We offer the vast selection of products that are vetted for the best quality that you’ll find
+                anywhere all at your fingertips.
             </v-col>
             <v-col cols="12" md="4" class="pa-8 text-justify">
-                Renting online in Malaysia has never been this easy! We will always have new offers and new deals available for you. Be in tuned with us by subscribing to our mailers. Remember, you can rent anything any day on RentSmart. Last but not the least, we want to emphasize that we’re more than just one of the Malaysia’s largest online eCommerce rental stores. It is our highest priority here at rentsmartasia.com to create the best online renting experience for every customer in Malaysia. So start renting online and start saving.
+                Renting online in Malaysia has never been this easy! We will always have new offers and new deals
+                available for you. Be in tuned with us by subscribing to our mailers. Remember, you can rent anything
+                any day on RentSmart. Last but not the least, we want to emphasize that we’re more than just one of the
+                Malaysia’s largest online eCommerce rental stores. It is our highest priority here at rentsmartasia.com
+                to create the best online renting experience for every customer in Malaysia. So start renting online and
+                start saving.
             </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script>
+import {get} from "vuex-pathify";
+
 export default {
     name: 'Home',
     data: () => ({
@@ -81,6 +275,21 @@ export default {
                 title: 'Earn from Dresses You Hardly Use on Malaysia\'s Largest Rental Platform'
             }
         ]
-    })
+    }),
+    computed: {
+        products: get('products/all'),
+
+        featured() {
+            return this._.sampleSize(this.products, 4)
+        },
+
+        top_items() {
+            return this._.sampleSize(this.products, 4)
+        },
+
+        new_releases() {
+            return this._.sampleSize(this.products, 4)
+        }
+    }
 }
 </script>
